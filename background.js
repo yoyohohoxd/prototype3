d5c82d7c51e42878e95f4a6ffedf6932d73683dc
Mads-Emil Kjeldsen Hansen <madse@live.dk>
2021-12-02 22:13:08 +0100

    start work on injection script

--- background.js
+++ background.js
@@ -4,3 +4,13 @@ chrome.runtime.onInstalled.addListener(() => {
     chrome.storage.sync.set({ color });
     console.log('Default background color set to %cgreen', `color: ${color}`);
 })
+
+chrome.runtime.onMessage.addListener(
+    function(message, callback) {
+        if (message == "changeColor"){
+            chrome.tabs.executeScript({
+                code: 'console.log("HI")'
+            });
+        }
+    }
+);
